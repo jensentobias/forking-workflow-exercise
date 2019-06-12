bd742cc7c015440133549d05ffd1b46a893be246
Tobias Lindstrøm Jensen <jensentobias@users.noreply.github.com>
2019-06-12 14:35:00 +0200

    Added unittest and script with nosetest

---
+++ inerror.py
@@ -0,0 +1,2 @@
+def tweet():
+    return "please replace something more fun with this boring sentence"*10
--- test.py
+++ test.py
@@ -4,11 +4,23 @@ tweet()-function returns a string that is less than 280 characters long.
 """
 
 import os
+import unittest
 
-for filename in os.listdir("."):
-    if filename.startswith("group") and filename.endswith(".py"):
-        module_name = filename[:-3]  # ignore .py at the end
-        module = __import__(module_name)
+class TestTweets(unittest.TestCase):
+
+    def test_smaller_than_280(self):
+
+        for filename in os.listdir("."):
+            if filename.startswith("group") and filename.endswith(".py"):
+                module_name = filename[:-3]  # ignore .py at the end
+                module = __import__(module_name)
+                tweet_content = module.tweet().encode('utf-8')
+                self.assertTrue(len(tweet_content) <= 280)
+
+    def test_larger_than_280(self):
+        module = __import__("inerror")
         tweet_content = module.tweet().encode('utf-8')
-        assert len(tweet_content) <= 280, \
-            "tweet is too long: '{}'".format(tweet_content)
+        self.assertFalse(len(tweet_content) <= 280)
+
+if __name__ == '__main__':
+    unittest.main()
